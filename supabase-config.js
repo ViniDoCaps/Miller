90f801c5dce65f39b2099daf5bda4ba88112dbf6
ViniDoCaps <goebelvinicius@gmail.com>
2026-08-25 12:03:57 -0400

    testesupabase

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 // Preencha com os dados públicos do seu projeto Supabase.
 // Use somente a chave anon/public; nunca coloque uma service_role key no site.
 window.ESPRESSO_SUPABASE = {
-  url: '',
-  anonKey: ''
+  url: 'https://mxhayhgfxfyefodmgshs.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14aGF5aGdmeGZ5ZWZvZG1nc2hzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2NzAwNDIsImV4cCI6MjEwMzI0NjA0Mn0.bdKYWIEppoBraKKdx3JThjiT-Lvau73cn1hoNplqID4'
 };
